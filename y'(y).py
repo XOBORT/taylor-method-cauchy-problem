1adb5efd5c45c323ece5dafd1ba2a04ed6e45037
Nikitin Evgeniy <62264919+XOBORT@users.noreply.github.com>
2020-05-12 09:49:59 +0300

    Add files via upload

--- y'(y).py
+++ y'(y).py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import pylab
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,8 +6,8 @@ pylab.grid(color="black", which="major", linewidth=1)
 pylab.grid(color="k", which="minor", linestyle=":", linewidth=0.5)
 data=np.loadtxt ("ans1.dat")
 plt.plot(data[:,1], data[:,2],color= 'b')
-plt.title(" y'(y) ")
-plt.ylabel("y(x)")
+plt.title(" Фазовая траектория ")
+plt.ylabel("y'(x)")
 plt.xlabel("x")
 
 pylab.show()
